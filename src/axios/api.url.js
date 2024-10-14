bf6fcce4a6818e6531b33604b4335691f35ba8c7
ayyouy <ayyouy@163.com>
2024-10-14 21:42:35 +0800

    update file upload

--- src/axios/api.url.js
+++ src/axios/api.url.js
@@ -1,8 +1,8 @@
 const ENV = process.env.NODE_ENV
 export default {
   // DOMAIN: 'http://121.40.17.234:8101/',
-  DOMAIN: 'http://103.214.142.174:8101/',
   // baseURL: 'http://121.40.17.234:8101/',
+  DOMAIN: 'http://103.214.142.174:8101/',
   baseURL: 'http://103.214.142.174:8101/',
   util: {
     image: '/util/image.html' // 图片上传
